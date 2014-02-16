25b97e33a1fe4d270b1863c189fe18adfda46a20
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-02-16 14:41:34 -0600

    Añadidos modelos de POST de Información de Contacto y Cuenta de Usuario.

---
+++ Kilometros WebAPI/Models/HttpPost/My_Controller/AccountPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Models.HttpPost.My_Controller {
+    public class AccountPost {
+        public string Email;
+        public string PreferredCultureCode;
+        public string RegionCode;
+        public string Password;
+    }
+}
---
+++ Kilometros WebAPI/Models/HttpPost/My_Controller/ContactInfoPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Models.HttpPost.My_Controller {
+    public class ContactInfoPost {
+        public string HomePhone;
+        public string MobilePhone;
+        public string WorkPhone;
+    }
+}
