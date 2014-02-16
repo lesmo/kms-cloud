96874e2efd7ebf6b0f5cc410dc2b858650d54635
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-02-16 14:40:57 -0600

    Añadido manejo de excepciones tipo "Not Modified"

---
+++ Kilometros WebAPI/ExceptionFilters/HttpNotModifiedExceptionFilter.cs
@@ -0,0 +1,21 @@
+﻿using Kilometros_WebAPI.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Filters;
+
+namespace Kilometros_WebAPI.ExceptionFilters {
+    public class HttpNotModifiedExceptionFilter : ExceptionFilterAttribute {
+        public override void OnException(HttpActionExecutedContext httpContext) {
+            if ( httpContext.Exception is HttpNotModifiedException ) {
+                httpContext.Response
+                    = new HttpResponseMessage(HttpStatusCode.NotModified);
+            }
+
+            base.OnException(httpContext);
+        }
+    }
+}
---
+++ Kilometros WebAPI/Exceptions/HttpNotModifiedException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Exceptions {
+    public class HttpNotModifiedException : Exception {
+    }
+}
