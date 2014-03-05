7baa651b88da0d8e452d10af78f4aaf84e09251f
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-03-05 15:48:11 -0600

    Añadido controlador para proceso OAuth de 3eros.

---
+++ Kilometros WebAPI/Controllers/OAuthThirdPartyController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Kilometros_WebAPI.Security;
+using KilometrosDatabase;
+using System.Web;
+using Kilometros_WebAPI.Models.HttpPost.SessionController;
+using Kilometros_WebGlobalization.API;
+using Kilometros_WebAPI.Models.HttpGet.SessionController;
+using System.Globalization;
+using Kilometros_WebAPI.Exceptions;
+using Kilometros_WebAPI.Helpers;
+using System.Text;
+
+namespace Kilometros_WebAPI.Controllers {
+    public class OAuthThirdPartyController : ApiController {
+        public KilometrosDatabase.Abstraction.WorkUnit Database
+            = new KilometrosDatabase.Abstraction.WorkUnit();
+
+        [HttpPost]
+        [Route("oauth/3rd/facebook")]
+        public void FacebookLogin([FromBody]string facebook_token) {
+            KmsIdentity identity
+                = (KmsIdentity)User.Identity;
+
+            if ( identity.UserData != null ) {
+            }
+
+            OAuthCredential oAuthCredential
+                = Database.OAuthCredentialStore.GetFirst(
+                    f => f.OAuthProvider == OAuthService.Facebook && f.Token == facebook_token
+                );
+
+            if ( oAuthCredential == null ) {
+                throw new HttpUnauthorizedException(
+                    "105" + ControllerStrings.Warning105_SocialTokenInvalid
+                );
+            }
+        }
+
+        [HttpPost]
+        [Route("oauth/3rd/twitter")]
+        public void TwitterLogin() {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("oauth/3rd/fitbit")]
+        public void FitbitLogin() {
+        }
+
+        [HttpPost]
+        [Route("oauth/3rd/nike")]
+        public void NikeLogin() {
+        }
+    }
+}
