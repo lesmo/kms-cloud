33a490f957bb960f9bb08be36634afe31ebd4cd8
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-03-27 16:08:31 -0600

    Añadido Modelo para Controladores OAuth

---
+++ Kilometros WebAPI/Models/RequestModels/OAuth3rdLoginPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Models.RequestModels {
+    public class OAuth3rdLoginPost {
+        /// <summary>
+        ///     Token de Sesión generado por el servicio OAuth de Terceros.
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        ///     Token Secret de Sesión generado por el servicio OAuth de Terceros.
+        ///     Los servicios que utilicen OAuth 2.0 no utilizan un Token Secret,
+        ///     y en esos casos debe omitirse.
+        /// </summary>
+        public string TokenSecret { get; set; }
+        /// <summary>
+        ///     ID del Usuario en el servicio OAuth de Terceros.
+        /// </summary>
+        public string SocialId { get; set; }
+    }
+}
---
+++ Kilometros WebAPI/Models/RequestModels/OAuthAccessTokenPost.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Models.RequestModels {
+    public class OAuthAccessTokenPost {
+        public string oauth_verifier {
+            get;
+            set;
+        }
+    }
+}
