71da51cdc156e1d0b5d8c4ae1fdbd2334c073b74
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-13 17:06:58 -0500

    Añadido controlador para obtener Recursos Dinámicos, almacenados en BD. Ahora sólo devuelve las imágenes almacenadas en BD.

---
+++ Kilometros WebApp/Controllers/DynamicResourcesController.cs
@@ -0,0 +1,31 @@
+﻿using KilometrosDatabase;
+using KilometrosDatabase.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kilometros_WebApp.Controllers {
+	public class DynamicResourcesController : BaseController {
+		//
+		// GET: /DynamicResources/
+		public BinaryResult Image(string filename, string ext) {
+			IPicture picture
+				= Database.IPictureStore.Get(filename);
+
+			if ( picture == null )
+				throw new HttpException(
+					404,
+					"Not Found"
+				);
+			else
+				return new BinaryResult() {
+					ContentType
+						= picture.PictureMimeType,
+					Content
+						= picture.Picture
+				};
+		}
+	}
+}
