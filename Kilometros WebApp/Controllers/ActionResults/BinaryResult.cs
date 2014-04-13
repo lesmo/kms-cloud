eb31b3faf2b85af4743826194bcb9d74cd2b6578
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-13 17:02:56 -0500

    Añadido ActionResult para respuesta de elementos binarios (imágenes, y archivos en general).

---
+++ Kilometros WebApp/Controllers/ActionResults/BinaryResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kilometros_WebApp.Controllers {
+    public class BinaryResult : ActionResult {
+        public byte[] Content {
+            get;
+            set;
+        }
+        
+        public string ContentType {
+            get;
+            set;
+        }
+
+        public override void ExecuteResult(ControllerContext context) {
+            context.HttpContext.Response.Clear(); 
+
+            context.HttpContext.Response.ContentType
+                = this.ContentType;
+            context.HttpContext.Response.OutputStream.Write(
+                this.Content,
+                0,
+                this.Content.Length
+            );
+        }
+    }
+}
