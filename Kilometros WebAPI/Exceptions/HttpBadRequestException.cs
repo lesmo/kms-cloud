87a55f9ce83099a3e4d94169e8b4397c77eb24fa
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-14 00:55:33 -0500

    Añadida excepción de BadRequest.

---
+++ Kilometros WebAPI/Exceptions/HttpBadRequestException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kilometros_WebAPI.Exceptions {
+    public class HttpBadRequestException : HttpProcessException {
+        public HttpBadRequestException(string message) : base(message) {}
+    }
+}
