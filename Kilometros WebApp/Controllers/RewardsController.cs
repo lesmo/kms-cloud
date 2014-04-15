1fd625a13bbb616fefa52cebb66c023a2736aac1
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-14 20:46:20 -0500

    Ammend de commit anterior.

---
+++ Kilometros WebApp/Controllers/RewardsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kilometros_WebApp.Controllers {
+	public class RewardsController : Controller {
+		// GET: /Rewards/
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
