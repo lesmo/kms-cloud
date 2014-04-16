b40c1d72e04e65fc98704a6f5cbdb0246d746aa5
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-16 04:37:03 -0500

    Proceso de Login finalizado.

--- Kilometros WebApp/Controllers/LoginController.cs
+++ Kilometros WebApp/Controllers/LoginController.cs
@@ -1,18 +1,48 @@
-﻿using System;
+﻿using KilometrosDatabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace Kilometros_WebApp.Controllers {
-	public class LoginController : Controller {
-		//
+	public class LoginController : BaseController {
 		// GET: /Login/
 		public ActionResult Index() {
 			return View();
 		}
 
-		public ActionResult Facebook() {
+		// POST: /Login/
+		[ValidateAntiForgeryToken]
+		[AcceptVerbs(HttpVerbs.Post)]
+		public ActionResult CreateSession(string email, string password, string returnUrl = null) {
+			if ( User.Identity.IsAuthenticated )
+				return Redirect("~/Overview");
+
+			// > Buscar al Usuario en BD por su Email
+			email
+				= email.ToLower();
+			User user
+				= Database.UserStore.GetFirst(
+					filter: f =>
+						f.Email == email.ToLower()
+				);
+
+			// > Validar que el Usuario exista y las contraseñas coincidan
+			if ( user == null || ! user.PasswordMatches(password) )
+				return View("Index", true);
+
+			// > Crear sesión y redirigir a donde aplique
+			FormsAuthentication.SetAuthCookie(user.Guid.ToBase64String(), true);
+			
+			if ( returnUrl == null )
+				return RedirectToAction("Index", "Overview");
+			else
+				return Redirect(returnUrl);
+		}
+
+		public ActionResult Facebook(string oauth_token) {
 			return View();
 		}
 
