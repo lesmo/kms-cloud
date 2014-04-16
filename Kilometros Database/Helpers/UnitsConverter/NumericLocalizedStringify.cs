d7c6335983b4400fa6529658a37b2697f67f4b36
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-15 19:36:10 -0500

    Añadidas funciones para dar formato a números, y mostrarlos según la configuración Cultural del Usuario.

---
+++ Kilometros Database/Helpers/UnitsConverter/NumericLocalizedStringify.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KilometrosDatabase.Helpers.UnitsConverter {
+    public static partial class UnitsConverter {
+        private static Func<dynamic, string> NumberToStringFunction
+            = f => ((Double)f).ToString("N", CultureInfo.CurrentCulture.NumberFormat);
+        private static Func<dynamic, string> CurrencyToStringFunction
+            = f => ((Double)f).ToString("C1", CultureInfo.CurrentCulture.NumberFormat);
+
+        public string ToLocalizedString(this Double @this) {
+            return NumberToStringFunction(@this);
+        }
+        public string ToLocalizedString(this Int64 @this) {
+            return NumberToStringFunction(@this);
+        }
+        public string ToLocalizedString(this Int32 @this) {
+            return NumberToStringFunction(@this);
+        }
+        public string ToLocalizedString(this Int16 @this) {
+            return NumberToStringFunction(@this);
+        }
+
+        public string ToCurrencyString(this Double @this) {
+            return CurrencyToStringFunction(@this);
+        }
+        public string ToCurrencyString(this Int64 @this) {
+            return CurrencyToStringFunction(@this);
+        }
+        public string ToCurrencyString(this Int32 @this) {
+            return CurrencyToStringFunction(@this);
+        }
+        public string ToCurrencyString(this Int16 @this) {
+            return CurrencyToStringFunction(@this);
+        }
+    }
+}
