c490aed4e7dd233b7f10d0182aa5e9b8ffbd03f4
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-17 17:09:09 -0500

    Modificado accessor de cadenas de Notificaciones de {internal} a {public}.

--- Kilometros WebGlobalization/Database/NotificationStrings.Designer.cs
+++ Kilometros WebGlobalization/Database/NotificationStrings.Designer.cs
@@ -22,7 +22,7 @@ namespace Kilometros_WebGlobalization.Database {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class NotificationStrings {
+    public class NotificationStrings {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace Kilometros_WebGlobalization.Database {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kilometros_WebGlobalization.Database.NotificationStrings", typeof(NotificationStrings).Assembly);
@@ -51,7 +51,7 @@ namespace Kilometros_WebGlobalization.Database {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace Kilometros_WebGlobalization.Database {
         /// <summary>
         ///   Looks up a localized string similar to Sent you a friend request.
         /// </summary>
-        internal static string SentYouAFriendRequest {
+        public static string SentYouAFriendRequest {
             get {
                 return ResourceManager.GetString("SentYouAFriendRequest", resourceCulture);
             }
