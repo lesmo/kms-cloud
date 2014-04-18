c06fdad6bac9530eeafdbdac3cde238221fe9f0c
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-04-18 02:38:15 -0500

    Refactor a conversión de divisas.

--- Kilometros Database/Helpers/CurrencyConverter.cs
+++ Kilometros Database/Helpers/CurrencyConverter.cs
@@ -11,46 +11,31 @@ namespace KilometrosDatabase.Helpers {
             = new Dictionary<String, Double> {
                 {"USD", 1},
                 {"MXN", 13.0d},
-                {"EUR", 0.7d}
+                {"EUR", 0.7d},
+                {"GBP", 0.6d}
             };
-        
-        private static Func<dynamic, RegionInfo, Double?> DollarsToRegionCurrencyFunction
-            = (dollars, region) => (
-                DollarEquivalence.ContainsKey(region.ISOCurrencySymbol.ToUpper())
-                ? DollarEquivalence[region.ISOCurrencySymbol.ToUpper()] * dollars
-                : null
-            );
 
-        public static Double? DollarsToRegionCurrency(this Double @this, RegionInfo region = null) {
-            if ( region == null )
-                region
-                    = RegionInfo.CurrentRegion;
-
-            return DollarsToRegionCurrencyFunction(@this, region);
+        private static Double DollarCentsToDollars(this IConvertible @this) {
+            return (Double)@this / 100d;
         }
 
-        public static Double? DollarsToRegionCurrency(this Int64 @this, RegionInfo region = null) {
-            if ( region == null )
-                region
-                    = RegionInfo.CurrentRegion;
-
-            return DollarsToRegionCurrencyFunction(@this, region);
+        private static Int32 DollarsToDollarCents(this IConvertible @this) {
+            return (Int32)((Double)@this * 100d);
         }
 
-        public static Double? DollarsToRegionCurrency(this Int32 @this, RegionInfo region = null) {
+        private static Double? DollarsToRegionCurrency(this IConvertible @this, RegionInfo region = null) {
             if ( region == null )
                 region
                     = RegionInfo.CurrentRegion;
 
-            return DollarsToRegionCurrencyFunction(@this, region);
+            if ( DollarEquivalence.ContainsKey(region.ISOCurrencySymbol.ToUpper()) )
+                return DollarEquivalence[region.ISOCurrencySymbol.ToUpper()] * (Double)@this;
+            else
+                return null;
         }
 
-        public static Double? DollarsToRegionCurrency(this Int16 @this, RegionInfo region = null) {
-            if ( region == null )
-                region
-                    = RegionInfo.CurrentRegion;
-
-            return DollarsToRegionCurrencyFunction(@this, region);
+        public static Double? DollarCentsToRegionCurrency(this IConvertible @this, RegionInfo region = null) {
+            return @this.DollarCentsToDollars().DollarsToRegionCurrency(region);
         }
     }
 }
