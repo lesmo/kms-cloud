704890cb24dd9ad2836f14e380b723c29df2bde4
Sinuhé Coronel <lesmo@sharpdnm.com>
2014-05-20 02:47:18 -0500

    Añadida funcionalidad de obtención de catálogo geográfico mundial.

---
+++ Kms Cloud Api/Controllers/WorldController.cs
@@ -0,0 +1,31 @@
+﻿using Kms.Cloud.Api.Models.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Kms.Cloud.Api.Controllers {
+    [AllowAnonymous]
+    public class WorldController : BaseController {
+        [HttpGet, Route("world")]
+        public IEnumerable<WorldCountryResponse> GetWorld() {
+            return Database.RegionStore.GetAll(
+                include:
+                    new string[] { "RegionSubdivision" }
+            ).Select(r =>
+                new WorldCountryResponse {
+                    Code = r.IsoCode,
+                    Name = r.Name,
+                    Subdivisions = r.RegionSubdivision.Select(s =>
+                        new WorldSubdivisionResponse {
+                            Code = s.IsoCode,
+                            Name = s.Name
+                        }
+                    )
+                }
+            );
+        }
+    }
+}
---
+++ Kms Cloud Api/Models/ResponseModels/WorldCountryResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kms.Cloud.Api.Models.ResponseModels {
+    public class WorldSubdivisionResponse {
+        public String Code { get; set; }
+        public String Name { get; set; }
+    }
+
+    public class WorldCountryResponse : WorldSubdivisionResponse {
+        public IEnumerable<WorldSubdivisionResponse> Subdivisions { get; set; }
+    }
+}
