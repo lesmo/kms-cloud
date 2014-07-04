fb655765d6945598d6dcdfbd262177f637963e17
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-07-03 23:34:10 -0500

    Añadido helper para obtener dirección IP de cliente.

--- Kms Cloud Api/GlobalExtensions.cs
+++ Kms Cloud Api/GlobalExtensions.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
+using System.ServiceModel.Channels;
 using System.Web;
 
 namespace Kms.Cloud.Api {
@@ -14,6 +15,19 @@ namespace Kms.Cloud.Api {
             taskCompletion.SetResult(@this);
 
             return taskCompletion.Task;
+        }
+
+        public static String GetClientIpAddress(this HttpRequestMessage request) {
+            // Source: http://forums.asp.net/post/4855159.aspx
+            if ( request.Properties.ContainsKey("MS_HttpContext") ) {
+                return ((HttpContext)request.Properties["MS_HttpContext"]).Request.UserHostAddress;
+            } else if ( request.Properties.ContainsKey(RemoteEndpointMessageProperty.Name) ) {
+                RemoteEndpointMessageProperty prop;
+                prop = (RemoteEndpointMessageProperty)request.Properties[RemoteEndpointMessageProperty.Name];
+                return prop.Address;
+            } else {
+                return String.Empty;
+            }
         }
     }
 }
