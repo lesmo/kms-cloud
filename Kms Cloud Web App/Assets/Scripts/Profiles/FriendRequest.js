3f9d532899b3761351c7fe70daff8860c8b797c7
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-07-16 00:16:43 -0500

    Añadidos estilos y funcionalidad a botón de añadir amigo.

---
+++ Kms Cloud Web App/Assets/Scripts/Profiles/FriendRequest.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../Shared/_shared.js" />
+
+var added = false;
+
+function doKMS_friendRequest_send(friendId) {
+    if (added)
+        return;
+
+    var $button = $(this);
+    $button.addClass('disabled').attr('href', '').text('Enviando solicitud ...');
+
+    $.post(getKMS_ajaxUri("FriendRequestAccept.json"), {
+        friendId: friendItem.data('uid')
+    }).fail(function () {
+        console.log("[!] - Error de descarga de datos");
+    }).done(function () {
+        $button.text('Solicitud enviada');
+    });
+}
