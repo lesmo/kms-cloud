e0b8501710f7c95ff8dc48af3e021bc0e8774f2c
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-08-06 22:51:38 -0500

    Añadido helper de conversión de DateTime a Epoch de Javascript.

--- Kms Cloud Database/Helpers/UnitsConverter/UnitsConverter.cs
+++ Kms Cloud Database/Helpers/UnitsConverter/UnitsConverter.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Kms.Cloud.Database.Helpers {
     public static partial class UnitsConverter {
+        
+
         internal static Double ToDouble(this IConvertible @this) {
             return @this.ToDouble(System.Globalization.NumberFormatInfo.InvariantInfo);
+        }
+
+        private const Int64 LinuxEpochTicks = 621355968000000000;
+
+        public static Int64 ToJavascriptEpoch(this DateTime @this) {
+            return @this.AddTicks(-LinuxEpochTicks).Ticks / 10000;
         }
     }
 }
